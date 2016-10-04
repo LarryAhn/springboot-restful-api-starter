e509753c1cf0b510d14268161302967987b8ebf2
Lawrence Ahn <lawrenceahn27@gmail.com>
2016-10-04 20:07:20 +0900

    spring profile 적용 application.yml ...

--- src/main/java/com/zuperztarahn/Application.java
+++ src/main/java/com/zuperztarahn/Application.java
@@ -1,12 +1,41 @@
 package com.zuperztarahn;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
-public class Application {
+@RestController
+@PropertySource(value = "classpath:/config/application.yml")
+public class Application extends SpringBootServletInitializer {
+
+	@Value("${enviroment}")
+	private String env;
+
+	@PostConstruct
+	public void postConstruct() {
+		System.out.println("Application started profile : " + env);
+	}
+
+	@GetMapping
+	public String index() {
+		return env;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
 }
